f879bb2de0261f493fc20a8314e065de690e3073
Ganesh Durai <ganiths@gmail.com>
2020-04-14 20:25:23 +0100

    Add dummy webservice for receiving help request

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const express = require('express');
 const app = express();
 
+var assistance = require('./assistance');
+
+app.use('/assistance', assistance);
+
 app.listen(3000, () => console.log('Listening'));
---
+++ assistance.js
@@ -0,0 +1,10 @@
+var express = require('express')
+var router = express.Router()
+
+// define the https post for storing assistance request
+router.post('/', function (req, res) {
+    //TODO Call database and store the assistance request
+    res.send({'IsMessageReceived': true});    
+  })
+
+module.exports = router
