ed2d3b77c30257ad4ffe1c04b121d51ab1f9a6c2
Ganesh Durai <ganiths@gmail.com>
2020-04-15 07:55:37 +0100

    Enabled deployment to IBM cloud using starter kit

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-var assistance = require('./assistance');
-
-app.use('/assistance', assistance);
-
-app.listen(3000, () => console.log('Listening'));
