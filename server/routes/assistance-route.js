d621fb3b73679009ec2f2f0aedb28bc1e91248d7
Ganesh Durai <ganiths@gmail.com>
2020-04-20 08:33:10 +0100

    Modified the get route for assistance

--- server/routes/assistance-route.js
+++ server/routes/assistance-route.js
@@ -8,6 +8,6 @@ router.post("/", AssistanceController.storeAssistanceRequest);
 // define the https post for storing assistance request
 router.post("/acceptRequest", AssistanceController.acceptRequest);
 
-router.get("/getAssistance", AssistanceController.getAssistance);
+router.get("/", AssistanceController.getAssistance);
 
 module.exports = router;
